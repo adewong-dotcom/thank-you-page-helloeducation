8f418ffcce83fae2bef7b54ecbfdc5627c8bee08
adewong-dotcom <87203269+adewong-dotcom@users.noreply.github.com>
2026-09-19 00:41:17 -0600

    Update Google Form responder URLs in config.js

--- thank-you-assets/config.js
+++ thank-you-assets/config.js
@@ -1,6 +1,6 @@
 /* Add the published RESPONDER URLs from Google Forms, not editor URLs.
    This file is public: do not put passwords, API keys or student data here. */
 window.HELLO_ENROLLMENT_FORMS = Object.freeze({
-  es: '', // PASTE THE SPANISH GOOGLE FORM RESPONDER URL HERE
-  en: ''  // PASTE THE ENGLISH GOOGLE FORM RESPONDER URL HERE
+  es: 'https://forms.gle/2oQbwcqFjCwYuTPD8', // PASTE THE SPANISH GOOGLE FORM RESPONDER URL HERE
+  en: 'https://forms.gle/RcdczN8USE9NygZeA'  // PASTE THE ENGLISH GOOGLE FORM RESPONDER URL HERE
 });
