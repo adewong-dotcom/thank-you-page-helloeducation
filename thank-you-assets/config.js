aa9c77b3216dc84dea38bc76f08e6e8834697ea1
adewong-dotcom <87203269+adewong-dotcom@users.noreply.github.com>
2026-09-23 20:03:26 -0600

    Delete thank-you-assets directory

--- thank-you-assets/config.js
+++
@@ -1,6 +0,0 @@
-/* Add the published RESPONDER URLs from Google Forms, not editor URLs.
-   This file is public: do not put passwords, API keys or student data here. */
-window.HELLO_ENROLLMENT_FORMS = Object.freeze({
-  es: 'https://forms.gle/2oQbwcqFjCwYuTPD8', // PASTE THE SPANISH GOOGLE FORM RESPONDER URL HERE
-  en: 'https://forms.gle/RcdczN8USE9NygZeA'  // PASTE THE ENGLISH GOOGLE FORM RESPONDER URL HERE
-});
